bc8ab07c625cb764b96e282e82a57e5355fd9ba2
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2023-10-28 15:47:35 -0300

    added input handling system and updated movement
    Created a simple input handler to set some flags on a
    controller structure and used it to get constant input
    for the movement logic. Added the movement logic.
    TODO: Refactor code to other files, myabe an input file.

--- src/main.c
+++ src/main.c
@@ -14,24 +14,37 @@ typedef struct player_struct
   float delta_x;
   float delta_y;
   float acceleration;
+  float friction;
   SDL_FRect p_rect;
   SDL_Color p_color;
 } player;
 
-player create_player(float x, float y, float w, float h, float acc);
+typedef struct Input
+{
+  bool left;
+  bool right;
+  bool up;
+  bool down;
+}Input;
+
+player create_player(float x, float y, float w, float h, float acc, float frict);
 void draw_player(SDL_Renderer* renderer,player player);
+void update_player(player* player, Input controller, float deltaTime);
+void handle_input(SDL_Event event, Input* controller);
 
 int main(int argc, char *argv[])
 {
   SDL_Window *gWindow = NULL;
   SDL_Renderer *renderer = NULL;
 
+  Input controller = {0,0,0,0};
+
   if (!init(&gWindow, &renderer))
   {
     return -1;
   }
 
-  player p1 = create_player(100,70,10,10,100); 
+  player p1 = create_player(100,70,10,10,10,0.9); 
   
   float deltaTime = 0;
   Uint32 last_step = SDL_GetTicks();
@@ -39,49 +52,29 @@ int main(int argc, char *argv[])
   SDL_Event game_event;
   
   bool game = true;
-  
   while (game)
   {
     // Game Loop
     Uint32 current_step = SDL_GetTicks();
+    bool key_pressed = false;
     if (last_step < current_step)
     {
       deltaTime = get_deltaTime(current_step, last_step);
       // Game Event Loop
       while(SDL_PollEvent(&game_event))
       {
-        if (game_event.type == SDL_QUIT){
-          game = false;
+        switch (game_event.type){
+          case SDL_QUIT:
+            game = false;
+            break;
         }
-        else if (game_event.type == SDL_KEYDOWN)
-        {
-          if (game_event.key.keysym.sym == SDLK_LEFT)
-          {
-              p1.delta_x -= p1.acceleration * deltaTime;
-          }
-          if (game_event.key.keysym.sym == SDLK_RIGHT)
-          {
-              p1.delta_x += p1.acceleration * deltaTime;
-          }
-          if (game_event.key.keysym.sym == SDLK_UP)
-          {
-              p1.delta_y -= p1.acceleration * deltaTime;
-          }
-          if (game_event.key.keysym.sym == SDLK_DOWN)
-          {
-              p1.delta_y += p1.acceleration * deltaTime;
-          }
-        }
+
+        handle_input(game_event, &controller);
       }
-      // Update
-      float friction = 0.99;
-      p1.pos_x += p1.delta_x;
-      p1.pos_y += p1.delta_y;
-      p1.delta_x *= friction;
-      p1.delta_y *= friction;
       
-      p1.p_rect.x = p1.pos_x;
-      p1.p_rect.y = p1.pos_y;
+      //Update
+      update_player(&p1, controller, deltaTime);
+
       // Draw
       draw_game(renderer);
       draw_player(renderer, p1);
@@ -95,11 +88,71 @@ int main(int argc, char *argv[])
   return 0;
 }
 
+void handle_input(SDL_Event event, Input* controller)
+{
+  if (event.type == SDL_KEYDOWN)
+  {
+    switch (event.key.keysym.sym)
+    {
+      case SDLK_LEFT:
+      {
+        controller->left = true;
+        break;
+      }
+      case SDLK_RIGHT:
+      {
+        controller->right = true;
+        break;
+      }
+      case SDLK_UP:
+      {
+        controller->up = true;
+        break;
+      }
+      case SDLK_DOWN:
+      {
+        controller->down = true;
+        break;
+      }
+      default:
+        break;
+    }
+  }
+  else if (event.type == SDL_KEYUP)
+  {
+    switch (event.key.keysym.sym)
+    {
+      case SDLK_LEFT:
+      {
+        controller->left = false;
+        break;
+      }
+      case SDLK_RIGHT:
+      {
+        controller->right = false;
+        break;
+      }
+      case SDLK_UP:
+      {
+        controller->up = false;
+        break;
+      }
+      case SDLK_DOWN:
+      {
+        controller->down = false;
+        break;
+      }
+      default:
+        break;
+    }
+  }
+}
+
 /********************
  * Initializes a new player type with predefined 
  * variables and returns the player type.
 */
-player create_player(float x, float y, float w, float h, float acc)
+player create_player(float x, float y, float w, float h, float acc, float frict)
 {
   player new;
   // Player
@@ -108,6 +161,7 @@ player create_player(float x, float y, float w, float h, float acc)
   new.delta_x = 0;
   new.delta_y = 0;
   new.acceleration = acc;
+  new.friction = frict;
 
   // Rect
   new.p_rect.x = new.pos_x;
@@ -129,7 +183,30 @@ void draw_player(SDL_Renderer* renderer, player player)
   draw_rect(renderer, &player.p_rect, player.p_color);
 }
 
-void update_player(player player)
+void update_player(player* player, Input controller, float deltaTime)
 {
+  if (controller.left)
+  {
+    player->delta_x -= player->acceleration * deltaTime;
+  }
+  if (controller.right)
+  {
+    player->delta_x += player->acceleration * deltaTime;
+  }
+  if (controller.up)
+  {
+    player->delta_y -= player->acceleration * deltaTime;
+  }
+  if (controller.down)
+  {
+    player->delta_y += player->acceleration * deltaTime;
+  }
 
+  player->pos_x += player->delta_x;
+  player->delta_x *= player->friction;
+  player->p_rect.x = player->pos_x;
+
+  player->pos_y += player->delta_y;
+  player->delta_y *= player->friction;
+  player->p_rect.y = player->pos_y;
 }
